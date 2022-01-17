f59678a199ecb09708e9e72c5a0cc6521745a60e
Matteo Briani <matteo-briani@users.noreply.github.com>
2022-01-17 22:27:16 +0100

    add Celery and backend-worker service
    This add Celery and a backend-worker service where Celery is running.
    At this point in commit history, a celery task exists and it is called
    "debug_taks". In order to test, it is possible to enter in the Django
    shell by issuing "docker-compose exec backend-worker-dcf bash", then
    "python manage.py shell".
    The task is then call from the following code:
    ```
    from dcf.celery import debug_task
    debug_task.delay()
    ```

---
+++ services/backend/code/dcf/celery.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+import os
+from celery import Celery
+from .settings import CELERY_RESULT_BACKEND
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcf.settings')
+app = Celery('dcf', backend=CELERY_RESULT_BACKEND)
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
--- services/backend/code/dcf/settings.py
+++ services/backend/code/dcf/settings.py
@@ -129,3 +129,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CELERY related settings
+
+CELERY_BROKER_URL = 'redis://redis-dcf:6379'
+CELERY_RESULT_BACKEND = 'redis://redis-dcf:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
