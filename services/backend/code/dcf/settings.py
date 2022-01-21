2d12d82926eee711f31e2af0004eac35e1ec1384
Matteo Briani <matteo-briani@users.noreply.github.com>
2022-01-21 17:32:03 +0100

    Move Celery urls to env vars
    CELERY_BROKER_URL and CELERY_RESULT_BACKEND can be set from the
    docker-compose environment variables

--- services/backend/code/dcf/settings.py
+++ services/backend/code/dcf/settings.py
@@ -132,8 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CELERY related settings
 
-CELERY_BROKER_URL = 'redis://redis-dcf:6379'
-CELERY_RESULT_BACKEND = 'redis://redis-dcf:6379'
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379')
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://redis:6379')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
